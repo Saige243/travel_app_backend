b91d1558cfc43b1985a482cca10b645bfa18d7b1
Saige Cross <saigecrossdev@gmail.com>
2024-03-11 11:53:18 -0500

    added fn/ln to register

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,21 @@ Rails.application.routes.draw do
     resources :itinerary_items, only: [:index, :create, :update, :destroy]
   end
 
-  # Health check route
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Devise routes for user authentication
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   # User profile routes
   get 'profile', to: 'users#show'
   patch 'profile', to: 'users#update'
 
-    # Authentication check route
-    get 'auth/logged_in', to: 'authentication#logged_in'
+  # Authentication check route
+  get 'auth/logged_in', to: 'authentication#logged_in'
+
+  # Health check route
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Optionally define a root path
   # root "some_controller#index"
